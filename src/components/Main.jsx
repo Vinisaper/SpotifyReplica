ad47c67c1d008ce95415131f578700df85725d74
Vinisaper <vinisp@austertecnologia.com>
2025-02-11 16:08:58 -0300

    Header and Main

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="main__texts">
+                <h1>Artistas Populares</h1>
+                <a className="main__link" href="/">
+                    Mostrar tudo
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
